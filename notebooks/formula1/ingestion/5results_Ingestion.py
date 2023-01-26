1e813f1eedc510d9a6b6c1e53ed95e68933297aa
UMAR FAROOQ <72467283+umarfarooq-git@users.noreply.github.com>
2023-01-26 01:22:04 +0100

    results data Ingestion

---
+++ notebooks/formula1/ingestion/5results_Ingestion.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType
+results_schema = StructType(fields=[StructField("resultId", IntegerType(), False),
+                                    StructField("raceId", IntegerType(), True),
+                                    StructField("driverId", IntegerType(), True),
+                                    StructField("constructorId", IntegerType(), True),
+                                    StructField("number", IntegerType(), True),
+                                    StructField("grid", IntegerType(), True),
+                                    StructField("position", IntegerType(), True),
+                                    StructField("positionText", StringType(), True),
+                                    StructField("positionOrder", IntegerType(), True),
+                                    StructField("points", FloatType(), True),
+                                    StructField("laps", IntegerType(), True),
+                                    StructField("time", StringType(), True),
+                                    StructField("milliseconds", IntegerType(), True),
+                                    StructField("fastestLap", IntegerType(), True),
+                                    StructField("rank", IntegerType(), True),
+                                    StructField("fastestLapTime", StringType(), True),
+                                    StructField("fastestLapSpeed", FloatType(), True),
+                                    StructField("statusId", StringType(), True)])
+results_df = spark.read \
+.schema(results_schema) \
+.json("/mnt/formula1dlake3/raw/results.json")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+results_with_columns_df = results_df.withColumnRenamed("resultId", "result_id") \
+                                    .withColumnRenamed("raceId", "race_id") \
+                                    .withColumnRenamed("driverId", "driver_id") \
+                                    .withColumnRenamed("constructorId", "constructor_id") \
+                                    .withColumnRenamed("positionText", "position_text") \
+                                    .withColumnRenamed("positionOrder", "position_order") \
+                                    .withColumnRenamed("fastestLap", "fastest_lap") \
+                                    .withColumnRenamed("fastestLapTime", "fastest_lap_time") \
+                                    .withColumnRenamed("fastestLapSpeed", "fastest_lap_speed") \
+                                    .withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+results_final_df = results_with_columns_df.drop(col("statusId"))
+results_final_df.write.mode("overwrite").partitionBy('race_id').parquet("/mnt/formula1dlake3/processed/results")
